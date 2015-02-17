00fd6a22f2f081ff08f2605c67d88379359ac7d4
gwest4 <frostboss41@comcast.net>
2015-02-16 20:14:54 -0600

    Update MadLibsHandler.java

--- MadLibsHandler.java
+++ MadLibsHandler.java
@@ -26,10 +26,67 @@ public class MadLibsHandler implements Runnable {
 		}
 	}
 
+
 	@Override
 	public void run() {
+		//Declare variables used to store client communications
+		String message;
+		int client_input = -1;
+		
+		while (client_input != 0) {
+			try {
+				output.writeUTF("Mode:\n\t1 - Play\n\t2 - Create\n\t3 - Read\n\t0 - Disconnect");
+				client_input= input.readInt();
+				
+				switch (client_input) {
+				case (1):
+					output.writeUTF("You are in \"Play\" mode.");
+					play();
+					break;
+				case (2):
+					output.writeUTF("You are in \"Create\" mode.");
+					create();
+					break;
+				case (3):
+					output.writeUTF("You are in \"Read\" mode.");
+					read();
+					break;
+				case (0):
+					output.writeUTF("Disconnecting...");
+					break;
+				default:
+					output.writeUTF("There is no option available for that input");
+				}
+		
+			} catch (IOException e) {
+				System.out.println("Exception: " + e.getClass().toString());
+				System.out.println("\t" + e.getMessage());
+			}
+		}
 		
 	}
+	
+	/**
+	 * Begins running "play" mode
+	 */
+	private void play() {
+		
+	}
+	
+	/**
+	 * Begins running "create" mode
+	 */
+	private void create() {
+		
+	}
+	
+	/**
+	 * Begins running "read" mode
+	 */
+	private void read() {
+		
+	}
+	
 	
 	public static void main (String[] args) {
 		//Usage
