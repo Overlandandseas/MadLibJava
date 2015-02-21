fd9a10336b7c0be0bb73fdee4141fcdc8e0469c3
Dan <daniel.love.jr@gmail.com>
2015-02-21 12:28:54 -0600

    Version 0.1
    Changes
    	-Adding base file to master from which to branch into developer.
    	-This file is empty.
    	-http://s.mlkshk-cdn.com/r/97VP

---
+++ MadLibsClient.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.*;
+
+public class MadLibsClient {
+
+    public static void main(String[] args) {
+        
+        //emtpy master for the purpose of develop branch
+    }
+}
