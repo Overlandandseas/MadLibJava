989ac2fc6e4c66129e967425b592d0241d30b5d4
gwest4 <frostboss41@comcast.net>
2015-02-21 18:06:51 -0600

    Updates to client and client handler

--- MadLibsClient.java
+++ MadLibsClient.java
@@ -1,35 +1,98 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class MadLibsClient {
 
-  public static void main (String[] args) {
+	public static void main(String[] args) {
 
-    // Make sure args are passed
-    if ( !(args.length >= 2) ) {
-      System.out.println("Usage:");
-      System.out.println("     java MadLibsClient");
-      return;
-    }
+		// Make sure args are passed
+		if (!(args.length >= 2)) {
+			System.out.println("Usage:");
+			System.out.println("     java MadLibsClient hostname port");
+			System.out.println("          hostname:String");
+			System.out.println("               Use \"localhost\" to connect on loopback address");
+			System.out.println("          port:int");
 
-    // Various variables
-    String hostName = args[0];
-    int portNumber = Integer.parseInt(args[1]);
-    Socket MadSocket = null;
-    DataInputStream input;
-    DataOutputStream output;
+			return;
+		}
 
-    // Trying client type things, weep softly into my pillow
-    try {
-      MadSocket = new Socket (hostName, 19999);
-      input = new DataInputStream(MadSocket.getInputStream());
-      output = new DataOutputStream(MadSocket.getOutputStream());
-    }
-    catch (UnknownHostException e) {
-      System.err.println("Don't know about that host: " + hostName);
-    }
-    catch (IOException e) {
-        System.err.println("Couldn't get I/O for the connection to: " + hostName);
-    }
-  }
+		// Get variables from args
+		String hostName = args[0]; // Use "localhost" when testing with the server running locally
+		int portNumber = Integer.parseInt(args[1]);
+
+		// Declare variables to be used for connection
+		Socket MadSocket = null;
+		DataInputStream input;
+		DataOutputStream output;
+
+		try {
+			MadSocket = new Socket(hostName, portNumber);
+			input = new DataInputStream(MadSocket.getInputStream());
+			output = new DataOutputStream(MadSocket.getOutputStream());
+			//int client_input;
+			
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about that host: " + hostName);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: "
+					+ hostName);
+		}
+	}
+	
+	private static int chooseMode(DataInputStream input, DataOutputStream output) throws IOException{
+		int mode_int = 1;
+		int check = 0;
+		String message;
+		Scanner sc = new Scanner(System.in);
+		
+		while ( check == 0 ) { // While the client hasn't chosen the disconnect option
+			// Get message (game mode options) from server and print to screen
+			message = input.readUTF();
+			System.out.println(message);
+			
+			// Get int from the user (mode choice)
+			mode_int = sc.nextInt();
+			output.writeInt(mode_int);
+			
+			if ( (check=input.readInt()) == 0 ) { // Check that the mode option is a valid choice
+				// NOTE: All checks are done on the server.
+				//	     If a bad argument is sent, then check (an int read from the server) will not be 0
+				message = input.readUTF();
+				System.out.println(message);
+				
+				sc.close();
+				return mode_int;
+			} else {
+				message = input.readUTF();
+				System.out.println(message);
+			}
+		}
+		sc.close();
+		return -1;
+	}
+	
+	private static void beginPlayMode (DataInputStream input, DataOutputStream output) throws IOException {
+		String message;
+
+		// Get message (game mode confirmation) from server and print to screen
+		message = input.readUTF();
+		System.out.println(message);
+		
+		// Get message (exiting mode confirmation) from server and print to screen
+		message = input.readUTF();
+		System.out.println(message);
+	}
+	
+	private void beginCreateMode (DataInputStream input, DataOutputStream output) throws IOException {
+	
+	}
+	
+	private void beginReadMode (DataInputStream input, DataOutputStream output) throws IOException {
+		
+	}
+	
+	private static void disconnect() {
+		
+	}
 }
--- MadLibsHandler.java
+++ MadLibsHandler.java
@@ -1,6 +1,6 @@
 /*
  * Hey Dan, wassup. Your work for the first milestone is with the client.
- * Start by looking at line 42
+ * Start by looking at line 66
  */
 
 import java.io.DataInputStream;
@@ -8,106 +8,203 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-
 public class MadLibsHandler implements Runnable {
 	DataOutputStream output;
 	DataInputStream input;
 	Socket remote_socket;
-	
+
 	/**
 	 * Constructor
+	 * 
 	 * @param remote_socket
 	 */
-	public MadLibsHandler (Socket remote_socket) {
+	public MadLibsHandler(Socket remote_socket) {
 		this.remote_socket = remote_socket;
 		try {
-			//Get input/output streams
+			// Get input/output streams
 			this.input = new DataInputStream(remote_socket.getInputStream());
 			this.output = new DataOutputStream(remote_socket.getOutputStream());
-			
+
 		} catch (IOException e) {
 			System.out.println("Exception: " + e.getClass().toString());
 			System.out.println("\t" + e.getMessage());
 		}
 	}
 
-
 	@Override
 	public void run() {
-		//Declare variables used to store client communications
-		String message;
-		int client_input = -1;
+		long id = Thread.currentThread().getId();
 		
-		/*
-		 * Dan, the connection with the client starts here!
-		 * Pay special attention to the order in which the server writes/reads to the client socket
-		 */
+		int disconnect_status = -1;
+		int mode;
 		
-		while (client_input != 0) {
+		mode = chooseMode();
+		while (mode > 0) {
 			try {
-				//Write the game mode options to the remote socket
-				output.writeUTF("Mode:\n\t1 - Play\n\t2 - Create\n\t3 - Read\n\t0 - Disconnect");
-				
-				//Read int from client
-				client_input= input.readInt();
-				
-				//Depending on the int read, enter app mode accordingly
-				switch (client_input) {
+				switch (mode) {
 				case (1):
-					output.writeUTF("You are in \"Play\" mode.");
-					play();
+					beginPlayMode();
 					break;
 				case (2):
-					output.writeUTF("You are in \"Create\" mode.");
-					create();
+					beginCreateMode();
 					break;
 				case (3):
-					output.writeUTF("You are in \"Read\" mode.");
-					read();
-					break;
-				case (0):
-					output.writeUTF("Disconnecting...");
+					beginReadMode();
 					break;
 				default:
-					output.writeUTF("There is no option available for that input");
+					throw new Exception("Thread[%d]: Error switching modes");
 				}
-		
 			} catch (IOException e) {
 				System.out.println("Exception: " + e.getClass().toString());
 				System.out.println("\t" + e.getMessage());
+			} catch (Exception e) {
+				System.out.println("Exception: " + e.getClass().toString());
+				System.out.println("\t" + e.getMessage());
 			}
+			mode = chooseMode();
 		}
 		
+		beginDisconnect();
+		disconnect_status = 0;
+		
+		// Print disconnect status before closing the thread
+		if (disconnect_status == 0) {
+			System.out.printf("Thread[%d]: Disconnected gracefully", id);
+		} else {
+			System.out.printf("Thread[%d]: Disconnected due to IOException\n",id);
+		}
+
 	}
 	
+	private int chooseMode () {
+		// Declare variables used to store client communications
+		int client_input = 1;
+
+		/*
+		 * Dan, the connection with the client starts here! Pay special
+		 * attention to the order in which the server writes/reads to the client
+		 * socket
+		 */
+
+		while ( (client_input >= 0) &&  (client_input < 4)) {
+			// Write the game mode options to the remote socket
+			sendString("Choose mode:\n\t1 - Play\n\t2 - Create\n\t3 - Read\n\t0 - Disconnect");
+
+			// Read int from client
+			client_input = receiveInt();
+
+			// Depending on the int read, enter app mode accordingly
+			switch (client_input) {
+			case (1):
+				sendInt(0);
+				sendString("You are in \"Play\" mode.");
+				return client_input;
+			case (2):
+				sendInt(0);
+				sendString("You are in \"Create\" mode.");
+				return client_input;
+			case (3):
+				sendInt(0);
+				sendString("You are in \"Read\" mode.");
+				return client_input;
+			case (0):
+				sendInt(0);
+				sendString("Disconnecting...");
+				return client_input;
+			default:
+				sendInt(1);
+				sendString("There is no option available for that input");
+				return -1;
+			}
+		}
+		return -1;
+	}
+
 	/**
 	 * Begins running "play" mode
 	 */
-	private void play() {
+	private int beginPlayMode() {
 		System.out.println("MODE NOT YET IMPLEMENTED");
 		System.out.println("Exiting mode...");
+		sendString("Exiting mode...");
+		return 0;
 	}
-	
+
 	/**
 	 * Begins running "create" mode
 	 */
-	private void create() {
+	private int beginCreateMode() {
 		System.out.println("MODE NOT YET IMPLEMENTED");
 		System.out.println("Exiting mode...");
+		sendString("Exiting mode...");
+		return 0;
 	}
-	
+
 	/**
 	 * Begins running "read" mode
 	 */
-	private void read() {
+	private int beginReadMode() {
 		System.out.println("MODE NOT YET IMPLEMENTED");
 		System.out.println("Exiting mode...");
+		sendString("Exiting mode...");
+		return 0;
 	}
 	
+	/**
+	 * Disconnects the client from the server, and cleans up
+	 */
+	private void beginDisconnect() {
+		sendString("Disconnecting...");
+	}
 	
-	public static void main (String[] args) {
-		//Usage
-		System.out.println("This class is meant to be used by a MadLibsServer object");
+	private int sendString(String s) {
+		try {
+			output.writeUTF(s);
+			return 0;
+		} catch (IOException e) {
+			System.out.println("Exception: " + e.getClass().toString());
+			System.out.println("\t" + e.getMessage());
+			return 1;
+		}
+	}
+	
+	private int sendInt(int i) {
+		try {
+			output.writeInt(i);
+			return 0;
+		} catch (IOException e) {
+			System.out.println("Exception: " + e.getClass().toString());
+			System.out.println("\t" + e.getMessage());
+			return 1;
+		}
+	}
+	
+	private Integer receiveInt() {
+		try {
+			int i = input.readInt();
+			return i;
+		} catch (IOException e) {
+			System.out.println("Exception: " + e.getClass().toString());
+			System.out.println("\t" + e.getMessage());
+			return null;
+		}
+	}
+	
+	private String receiveString() {
+		try {
+			String s = input.readUTF();
+			return s;
+		} catch (IOException e) {
+			System.out.println("Exception: " + e.getClass().toString());
+			System.out.println("\t" + e.getMessage());
+			return null;
+		}
+	}
+
+	public static void main(String[] args) {
+		// Usage
+		System.out
+				.println("This class is meant to be used by a MadLibsServer object");
 		return;
 	}
 }
